5279f66ef4b1469af065c7c23d620ef27e0a581a
Cheryl Lee <99976292+CherylL23@users.noreply.github.com>
2022-02-23 00:50:58 +0000

    spaces

--- Sketch.java
+++ Sketch.java
@@ -4,13 +4,16 @@ public class Sketch extends PApplet {
 	
   //size of the picture
   public void settings() {
+
     size(400, 400);
   }
   //bacground coloring
   public void setup() {
+
     background(0, 210, 252);
   }
   public void draw() {
+    
     //sun
     stroke(0);
     fill(240, 252, 0);
