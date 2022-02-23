1067268da99da3f4dcb34afe3c9812ba91e2d564
Cheryl Lee <>
2022-02-23 00:39:59 +0000

    added house and background

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,48 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  //size of the picture
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  //bacground coloring
   public void setup() {
-    background(210, 255, 173);
+    background(0, 210, 252);
   }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    //sun
+    stroke(0);
+    fill(240, 252, 0);
+    ellipse(70, 70, 100, 100);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //house drawing
+    stroke(0);
+    fill(207, 76, 33);
+    triangle(100, 200, 300, 200, 200, 100);
+
+    stroke(0);
+    fill(240, 235, 182);
+    rect(100, 200, 200, 150);
+    
+    stroke(0);
+    fill(31, 191, 76);
+    rect(0, 340, 400, 400);
+
+    //door
+    stroke(0);
+    fill(82, 79, 53);
+    rect(190, 230, 80, 110);
+
+    stroke(0);
+    fill(240, 252, 0);
+    ellipse(255, 300, 15, 15);
+    
+    //window
+    stroke(0);
+    fill(145, 229, 242);
+    rect(110, 230, 70, 60);
+
+
   }
-  
-  // define other methods down here.
 }
